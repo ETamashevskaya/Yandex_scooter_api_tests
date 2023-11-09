daf28442a751477bc30344d6ebb7f53d632f5abb
ETamashevskaya <148258709+ETamashevskaya@users.noreply.github.com>
2023-11-09 14:43:42 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://7b314ab2-fbd2-4cad-bf32-9ffcffe78440.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREAT_USER_ORDERS = "/api/v1/orders"
+TAKE_TRACK_ORDERS = "/v1/orders/track?t="
+
+
+
+
+
